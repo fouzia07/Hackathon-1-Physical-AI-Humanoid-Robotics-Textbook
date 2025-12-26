11fdc5585b2b645b47c545986e59ad2d1ff5789d
fouzia_alee <163680919+fouzia07@users.noreply.github.com>
2025-12-26 16:16:34 +0500

    Update docusaurus.config.ts

--- ai-textbook/docusaurus.config.ts
+++ ai-textbook/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://fouzia07.github.io',
+  url: 'https://fouzia07.github.io/Hackathon-1-Physical-AI-Humanoid-Robotics-Textbook/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/Hackathon-1-Physical-AI-Humanoid-Robotics-Textbook/',
